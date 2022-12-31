46c563ce104162821dcc4323adf103a5d2b85526
Dennys Alvarenga <dennysnascimento01@gmail.com>
2022-12-31 15:32:59 -0300

    dynamic notifications

--- src/views/projects/Formulario.vue
+++ src/views/projects/Formulario.vue
@@ -13,7 +13,8 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { useStore } from '@/store';
-import { ALTERA_PROJETO, ADICIONA_PROJETO } from '@/store/tipo-mutacoes';
+import { ALTERA_PROJETO, ADICIONA_PROJETO, NOTIFICAR } from '@/store/tipo-mutacoes';
+import { TipoNotificacao } from '@/interfaces/INotificacao';
 
 export default defineComponent({
     name: 'Formulario',
@@ -44,6 +45,11 @@ export default defineComponent({
                 this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
             }
             this.nomeDoProjeto = '';
+            this.store.commit(NOTIFICAR, {
+                titulo: 'Novo projeto foi salvo',
+                texto: 'Prontinho ;) seu projeto já está disponível.',
+                tipo: TipoNotificacao.SUCESSO
+            })
             this.$router.push('/projetos');
         }
     },
